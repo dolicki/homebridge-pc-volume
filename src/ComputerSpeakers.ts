ec7fbfa124df0f53dadcd6bdf46029f094f49976
Goran Dolički <gorandolicki@gmail.com>
2023-06-29 18:09:57 +0200

    remove loudness

--- src/ComputerSpeakers.ts
+++ src/ComputerSpeakers.ts
@@ -15,7 +15,7 @@ export default class ComputerSpeakers {
   public async getMuted(): Promise<boolean> {
     this.log.debug(`Getting muted status`)
     try {
-      const isMuted = await this.loudness.getMuted()
+      const isMuted = false
       this.log.debug(`Got muted status: ${isMuted}`)
       return isMuted
     } catch (error) {
@@ -27,16 +27,7 @@ export default class ComputerSpeakers {
   public setMuted(newValue: boolean): Promise<void> {
     return new Promise((resolve, reject) => {
       this.log.debug(`Setting muted status to ${newValue}`)
-      this.loudness
-        .setMuted(newValue)
-        .then(() => {
-          this.log.debug(`Set muted status to ${newValue}`)
-          resolve()
-        })
-        .catch((error) => {
-          this.log.error(`Failed to set muted status to ${newValue}: ${error}`)
-          reject(error)
-        })
+      resolve()
     })
   }
 
@@ -60,50 +51,14 @@ export default class ComputerSpeakers {
           }
         }
       })()
-
-      this.loudness
-        .setVolume(systemVolume)
-        .then(() => {
-          this.log.debug(`Set volume to ${systemVolume}%`)
-          resolve(volume)
-        })
-        .catch((error) => {
-          this.log.error(`Failed to set volume to ${systemVolume}%: ${error}`)
-          reject(error)
-        })
+      resolve(volume)
     })
   }
 
   public getVolume(algorithm: VolumeAlgorithm): Promise<number> {
     return new Promise((resolve, reject) => {
       this.log.debug(`Getting volume`)
-
-      this.loudness
-        .getVolume()
-        .then((volume) => {
-          switch (algorithm) {
-            case VolumeAlgorithm.Linear:
-              this.log.debug(`Got system volume: ${volume}%`)
-              resolve(volume)
-              break
-            case VolumeAlgorithm.Logarithmic: {
-              const homekitVolume = Math.round(
-                Math.pow(10, volume / (100 / Math.log10(101))) - 1
-              )
-
-              this.log.debug(
-                `Converted system volume ${volume}% to ${homekitVolume} due to logarithmic volume algorithm`
-              )
-
-              resolve(volume)
-              break
-            }
-          }
-        })
-        .catch((error) => {
-          this.log.debug(`Failed to get volume: ${error}`)
-          reject(error)
-        })
+      resolve(Math.ceil(Math.random() * 100))
     })
   }
 
